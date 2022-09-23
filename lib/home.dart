c6f9ae0c1e487c8b7b4adb66e40a09002e424db7
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-09-23 12:34:44 +0545

    map type change done

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isSat = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
-      ),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(43.721998, -79.545075),
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          alignment: Alignment.topRight,
+          children: [
+            GoogleMap(
+              mapType: isSat ? MapType.satellite : MapType.normal,
+              initialCameraPosition: const CameraPosition(
+                zoom: 14.46,
+                target: LatLng(43.721998, -79.545075),
+              ),
+            ),
+            IconButton(
+              padding: const EdgeInsets.only(right: 20, top: 20),
+              onPressed: () {
+                setState(() {
+                  isSat = !isSat;
+                });
+              },
+              icon: Icon(
+                Icons.map,
+                size: 40,
+                color: isSat ? Colors.white : Colors.orange,
+              ),
+            ),
+          ],
         ),
       ),
     );
