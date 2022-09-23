6502c9007d7cbdc51c703a37011090944daf92dc
sthakrishnakumar <sthakrishnakumar@gmail.com>
2022-09-23 13:11:23 +0545

    move map location through button done

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,20 +11,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final Completer<GoogleMapController> _controller = Completer();
   bool isSat = false;
+  static const destiationCameraPosition = CameraPosition(
+    zoom: 14.46,
+    target: LatLng(43.721998, -79.545075),
+  );
+  static const currentCameraPosition = CameraPosition(
+    zoom: 14.46,
+    target: LatLng(27.704100, 85.322048),
+  );
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              children: [
+                FloatingActionButton(
+                  onPressed: gotoDes,
+                  child: const Icon(Icons.man),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                FloatingActionButton(
+                  onPressed: gotoPrev,
+                  child: const Icon(Icons.location_on),
+                ),
+              ],
+            ),
+          ),
+        ),
         body: Stack(
           alignment: Alignment.topRight,
           children: [
             GoogleMap(
+              onMapCreated: (GoogleMapController mapController) {
+                _controller.complete(mapController);
+              },
+              markers: const {},
               mapType: isSat ? MapType.satellite : MapType.normal,
-              initialCameraPosition: const CameraPosition(
-                zoom: 14.46,
-                target: LatLng(43.721998, -79.545075),
-              ),
+              initialCameraPosition: currentCameraPosition,
             ),
             IconButton(
               padding: const EdgeInsets.only(right: 20, top: 20),
@@ -40,6 +73,20 @@ class _HomeState extends State<Home> {
           ],
         ),
       ),
+    );
+  }
+
+  Future gotoDes() async {
+    GoogleMapController desController = await _controller.future;
+    desController.animateCamera(
+      CameraUpdate.newCameraPosition(destiationCameraPosition),
+    );
+  }
+
+  Future gotoPrev() async {
+    GoogleMapController prevController = await _controller.future;
+    prevController.animateCamera(
+      CameraUpdate.newCameraPosition(currentCameraPosition),
     );
   }
 }
